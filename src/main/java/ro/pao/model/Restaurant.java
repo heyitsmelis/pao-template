211d6567ac66b59f543e92c610aabf4684a4d9f0
Pintenaru Melissa <melissa.pintenaru05@gmail.com>
2023-04-01 16:12:36 +0300

    clase service (3rd commit)

--- src/main/java/ro/pao/model/Restaurant.java
+++ src/main/java/ro/pao/model/Restaurant.java
@@ -4,6 +4,8 @@ import lombok.*;
 import ro.pao.model.abstracts.AbstractEntity;
 import ro.pao.model.enums.RestaurantType;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,4 +21,6 @@ public class Restaurant extends AbstractEntity {
     private String shopAdress;
 
     private RestaurantType restaurantType;
+
+    private List<Food> meniuList;
 }
